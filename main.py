375de6ba927023e80039f3535883ada229abb07f
Orcanize <196217222+Orcanize@users.noreply.github.com>
2025-01-26 17:52:45 +0100

    completed the lesson and cleaned up the code

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ def main():
     count_letters = remove_non_letters(count_characters)
     letters_list = list_of_dict(count_letters)
 
-    #sort the list
+    #sort the list (does not need new variable, as it would then output "None" Grrrr)
     letters_list.sort(reverse=True, key=sort_on)
 
-
-    # text output of report
+    # text output start of report
     print(f"--- Begin report of {path} ---")
     print(f"{num_words} words found in the document")
     print("")
-    print(letters_list)
-
     
+    # print the whole report of each letter line
+    for values in letters_list:
+        print(f"The '{values['letter']}' character was found {values['count']} times")
+
+    # text output end of report
+    print("--- End report ---")
+
 
 # reads the book.txt file
 def get_book_text(path):
@@ -36,10 +40,11 @@ def get_count_characters(text):
         if char in dict:
             dict[char] += 1
         # adds characters not in dictionary
-        elif char not in dict:
+        else:
             dict[char] = 1
     return dict
 
+# new dictionary with only the letters
 def remove_non_letters(count_characters):
     letter_dict = {}
     letters = ("abcdefghijklmnopqrstuvwxyz") 
@@ -48,19 +53,16 @@ def remove_non_letters(count_characters):
                 letter_dict[char] = count_characters[char]
     return letter_dict
 
+# necessary to rearrange the dictionary into a nested one to sort
 def list_of_dict(count_letters):
     dict_list = []
     for letter in count_letters:
         dict_list.append({"letter" : letter, "count" : count_letters[letter]})
     return dict_list
 
+# necessary for the .sort function to know how to sort
 def sort_on(letters_list):
     return letters_list["count"]
-
-
-
-
-    
 
 
 main()
